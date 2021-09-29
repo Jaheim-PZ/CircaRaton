67b308e4211a9d60f33034c6cf11f9a3181b5e74
Beta-Soft <rebxcca93@gmail.com>
2021-09-29 08:46:03 -0400

    Added rooms to World/ Note to Jaheim: List class needs to be removed.

--- RatonLibrary/World.cs
+++ RatonLibrary/World.cs
@@ -18,7 +18,14 @@ namespace RatonLibrary
         public static bool exit = false;
         public static int index = 0;
 
-        public static List<string> rooms = new List<string>() { "Hollow Fairy Trunk", "Giant Gnome Garden", "Fire Creek Castle", "Shawdow Sirened Swamps", "Mystic Melody Forest" };
+        public static List<string> rooms = new List<string>() { "Hollow Fairy Trunk","Giant Gnome Garden", "Fire Creek Castle",
+                                                                "Shawdow Sirened Swamps", "Mystic Melody Forest","Hallway/Path: Harmony Hallows",
+                                                                "Hallway/Path: Squeaks Shakey Bridge", "Rusty Rogers Chili Can",
+                                                                "Swiss Roll Burrows", "Marios MatchBox Bank", "Luigis Pizzaria Pizza Box",
+                                                                "PopTart Plaza", "ToolBox Tavern", "Me Nifridge Melts",
+                                                                "Hallway/Path: Outlet Oasis", "Wall Cavity Falls", "Gutter Express",
+                                                                "Hallway/Path: Marios Casino Car Park", "Man Hole Hotel", "HoneySuckle Shrub Herbs" };
+
         public static List<string> weapons = new List<string>() { "Wretched Blade", "Pest Axe",
                                                                   "Stuarts Bow", "Whirly Twirly Saw" };
         public static List<string> potions = new List<string>() { "Healing", "Arcane" };
